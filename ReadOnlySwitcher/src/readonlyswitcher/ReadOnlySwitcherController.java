0b49c61dbb4955738abeeada179afc6215b32bed
Mehdi-Gh95 <87048088+Mehdi-Gh95@users.noreply.github.com>
2021-09-01 16:02:35 +0430

    Revert "Revert "fix""
    This reverts commit ba4f8d47e2ed5fd25262b7ec033595090cb04a02.

--- ReadOnlySwitcher/src/readonlyswitcher/ReadOnlySwitcherController.java
+++ ReadOnlySwitcher/src/readonlyswitcher/ReadOnlySwitcherController.java
@@ -66,12 +66,10 @@ public class ReadOnlySwitcherController implements Initializable {
         if (!listControl.contains("StorageDevicePolicies")) {
             text.setText(INTRO_ERROR);
         } else {
-            List<String> listStorageDevicePolicies = Arrays.asList(Advapi32Util.registryGetKeys(HKEY, REGISTER));
-            if (listStorageDevicePolicies.contains(DWORD)) {
+            if ( Advapi32Util.registryValueExists(HKEY, REGISTER, DWORD)) {
+               
                 value = Advapi32Util.registryGetIntValue(HKEY, REGISTER, DWORD);
             } else {
-//                Advapi32Util.registryCreateKey(HKEY, REGISTER, DWORD);
-//                Advapi32Util.registrySetStringValue(HKEY, REGISTER, DWORD, "1");
                 Advapi32Util.registrySetIntValue(HKEY, REGISTER, DWORD, 1);
                 value = Advapi32Util.registryGetIntValue(HKEY, REGISTER, DWORD);
             }
@@ -110,12 +108,10 @@ public class ReadOnlySwitcherController implements Initializable {
         Integer value;
         if (rOnly) {
             Advapi32Util.registrySetIntValue(HKEY, REGISTER, DWORD, 1);
-//            Advapi32Util.registrySetStringValue(HKEY, REGISTER, DWORD, "1");
             value = Advapi32Util.registryGetIntValue(HKEY, REGISTER, DWORD);
             return 1==value;
         } else {
             Advapi32Util.registrySetIntValue(HKEY, REGISTER, DWORD, 0);
-//            Advapi32Util.registrySetStringValue(HKEY, REGISTER, DWORD, "0");
             value = Advapi32Util.registryGetIntValue(HKEY, REGISTER, DWORD);
             return 0==value;
         }
